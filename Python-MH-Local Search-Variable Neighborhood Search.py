b74e6f015de6f2ecb3adad8a0e8a00582ba3bbc0
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-08-12 09:18:33 -0300

    Update Python-MH-Local Search-Variable Neighborhood Search.py

--- Python-MH-Local Search-Variable Neighborhood Search.py
+++ Python-MH-Local Search-Variable Neighborhood Search.py
@@ -13,16 +13,83 @@
 
 # Required Libraries
 import pandas as pd
+import numpy as np
 import random
 import copy
+from matplotlib import pyplot as plt 
 
-# Function: Distance
-def distance_calc(Xdata, route):
+# Function: Tour Distance
+def distance_calc(Xdata, city_tour):
     distance = 0
-    for k in range(0, len(route[0])-1):
+    for k in range(0, len(city_tour[0])-1):
         m = k + 1
-        distance = distance + Xdata.iloc[route[0][k]-1, route[0][m]-1]            
+        distance = distance + Xdata[city_tour[0][k]-1, city_tour[0][m]-1]            
     return distance
+
+# Function: Euclidean Distance 
+def euclidean_distance(x, y):       
+    distance = 0
+    for j in range(0, len(x)):
+        distance = (x[j] - y[j])**2 + distance   
+    return distance**(1/2) 
+
+# Function: Initial Seed
+def seed_function(Xdata):
+    seed = [[],float("inf")]
+    sequence = random.sample(list(range(1,Xdata.shape[0]+1)), Xdata.shape[0])
+    sequence.append(sequence[0])
+    seed[0] = sequence
+    seed[1] = distance_calc(Xdata, seed)
+    return seed
+
+# Function: Build Distance Matrix
+def buid_distance_matrix(coordinates):
+    Xdata = np.zeros((coordinates.shape[0], coordinates.shape[0]))
+    for i in range(0, Xdata.shape[0]):
+        for j in range(0, Xdata.shape[1]):
+            if (i != j):
+                x = coordinates[i,:]
+                y = coordinates[j,:]
+                Xdata[i,j] = euclidean_distance(x, y)        
+    return Xdata
+
+# Function: Tour Plot
+def plot_tour_distance_matrix (Xdata, city_tour):
+    m = np.copy(Xdata)
+    for i in range(0, Xdata.shape[0]):
+        for j in range(0, Xdata.shape[1]):
+            m[i,j] = (1/2)*(Xdata[0,j]**2 + Xdata[i,0]**2 - Xdata[i,j]**2)    
+    w, u = np.linalg.eig(np.matmul(m.T, m))
+    s = (np.diag(np.sort(w)[::-1]))**(1/2) 
+    coordinates = np.matmul(u, s**(1/2))
+    coordinates = coordinates.real[:,0:2]
+    xy = np.zeros((len(city_tour[0]), 2))
+    for i in range(0, len(city_tour[0])):
+        if (i < len(city_tour[0])):
+            xy[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][i]-1, 1]
+        else:
+            xy[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy[:,0], xy[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy[0,0], xy[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy[1,0], xy[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+    return
+
+# Function: Tour Plot
+def plot_tour_coordinates (coordinates, city_tour):
+    xy = np.zeros((len(city_tour[0]), 2))
+    for i in range(0, len(city_tour[0])):
+        if (i < len(city_tour[0])):
+            xy[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][i]-1, 1]
+        else:
+            xy[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy[:,0], xy[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy[0,0], xy[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy[1,0], xy[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+    return
 
 # Function: Stochastic 2_opt
 def stochastic_2_opt(Xdata, city_tour):
@@ -32,7 +99,7 @@ def stochastic_2_opt(Xdata, city_tour):
         i, j = j, i
     best_route[0][i:j+1] = list(reversed(best_route[0][i:j+1]))           
     best_route[0][-1]  = best_route[0][0]              
-    best_route[1] = distance_calc(Xdata, route = best_route)                     
+    best_route[1] = distance_calc(Xdata, best_route)                     
     return best_route
 
 # Function: Local Search
@@ -63,12 +130,38 @@ def variable_neighborhood_search(Xdata, city_tour, max_attempts = 20, neighbourh
                 best_solution = copy.deepcopy(solution) 
                 break
         count = count + 1
-        print("Iteration = ", count, "->", best_solution)
+        print("Iteration = ", count, "-> Distance ", best_solution[1])
     return best_solution
 
 ######################## Part 1 - Usage ####################################
 
-X = pd.read_csv('Python-MH-Local Search-Variable Neighborhood Search-Dataset-01.txt', sep = '\t') #17 cities = 2085
+# Load File - A Distance Matrix (17 cities,  optimal = 1922.33)
+X = pd.read_csv('Python-MH-Local Search-Variable Neighborhood Search-Dataset-01.txt', sep = '\t') 
+X = X.values
 
-cities = [[   1,  2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,   1   ], 4722]
-lsvns = variable_neighborhood_search(X, city_tour = cities, max_attempts = 25, neighbourhood_size = 5, iterations = 1000)
+# Start a Random Seed
+seed = seed_function(X)
+
+# Call the Function
+lsvns = variable_neighborhood_search(X, city_tour = seed, max_attempts = 25, neighbourhood_size = 5, iterations = 1000)
+
+# Plot Solution. Red Point = Initial city; Orange Point = Second City # The generated coordinates (2D projection) are aproximated, depending on the data, the optimum tour may present crosses
+plot_tour_distance_matrix(X, lsvns)
+
+######################## Part 2 - Usage ####################################
+
+# Load File - Coordinates (Berlin 52,  optimal = 7544.37)
+Y = pd.read_csv('Python-MH-Local Search-Variable Neighborhood Search-Dataset-02.txt', sep = '\t') 
+Y = Y.values
+
+# Build the Distance Matrix
+X = buid_distance_matrix(Y)
+
+# Start a Random Seed
+seed = seed_function(X)
+
+# Call the Function
+lsvns = variable_neighborhood_search(X, city_tour = seed, max_attempts = 25, neighbourhood_size = 5, iterations = 1000)
+
+# Plot Solution. Red Point = Initial city; Orange Point = Second City
+plot_tour_coordinates(Y, lsvns)
